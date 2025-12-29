970920136a3719a3a36489c47b4d972648b48a92
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-29 22:01:25 +0545

    Correct app configuration for apps.emails, apps.auth and apps.security modules

--- apps/auth/apps.py
+++ apps/auth/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class AuthConfig(AppConfig):
-    name = 'auth'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.auth"
--- apps/security/apps.py
+++ apps/security/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class SecurityConfig(AppConfig):
-    name = 'security'
+class AuthConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.security"
