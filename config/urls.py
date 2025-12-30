cda7d0a910206a205fc107f78cdce2b6cd146a4c
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-30 17:39:04 +0545

    Add user registration routes

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/auth/", include("apps.auth.urls")),
+    path('api/auth/', include("apps.auth.urls", namespace="auth")),
+    path('api/users/', include("apps.users.urls", namespace="users"))
 ]
