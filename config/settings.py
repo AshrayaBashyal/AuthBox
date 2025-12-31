5bb68a8fb2dbb256ae404de43bd94430c4810ea4
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-31 19:13:15 +0545

    Configure SMTP email settings for verification emails

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,18 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "users.User"
 
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
