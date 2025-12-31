d6f0c7f2a5322891fa1145c701bc7f9408adf234
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-31 18:57:08 +0545

    Block unverified users from logging in

--- apps/auth/serializers.py
+++ apps/auth/serializers.py
@@ -1,5 +1,13 @@
 from apps.auth.jwt import CustomTokenObtainPairSerializer
+from rest_framework.exceptions import AuthenticationFailed
 
 
 class LoginSerializer(CustomTokenObtainPairSerializer):
-    pass
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        if not self.user.is_verified:
+            raise AuthenticationFailed("Email not verified")
+
+        return data
