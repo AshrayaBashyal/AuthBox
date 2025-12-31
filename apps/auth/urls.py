2a7db3dda7d91a2ef7c10cb279b6cdb4cb3302ab
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-31 18:48:06 +0545

    Implement url for email verification

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, VerifyEmailView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 
@@ -8,4 +8,6 @@ app_name = "auth"
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path("refresh/", TokenRefreshView.as_view(), name='token_refresh'),
+    path("verify-email/", VerifyEmailView.as_view()),
+
 ]
