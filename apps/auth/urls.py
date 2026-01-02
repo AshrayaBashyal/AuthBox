764c762c67f8709203cf8b20a8054566ce2f2d1c
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-01-02 15:16:30 +0545

    feat(auth): add forgot-password and reset-password API routes

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import LoginView, VerifyEmailView
 from rest_framework_simplejwt.views import TokenRefreshView
-
+from ..users.views import ForgotPasswordView, ResetPasswordView
 
 app_name = "auth"
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path("refresh/", TokenRefreshView.as_view(), name='token_refresh'),
     path("verify-email/", VerifyEmailView.as_view()),
-
+    path("forgot-password/", ForgotPasswordView.as_view()),
+    path("reset-password/", ResetPasswordView.as_view()),
 ]
